f2795712ec70a841211bc6f95a9fffa5be910be4
MichaelRaatsPro <michaelraatspro@gmail.com>
2024-05-20 16:54:25 +0200

    added upload form

--- craft-connection/app/post/page.jsx
+++ craft-connection/app/post/page.jsx
@@ -1,21 +1,10 @@
 import React from 'react'
+import UploadFile from '../components/UploadFile'
 
 const page = () => {
   return (
-    <div style = {{display: 'flex', justifyContent: 'center' }} >
-      <div className ="formContainer" style = {{border: "solid black 3px", width: "20vw"}}>
-        <form>
-            <div className ="formGroup">
-                <label for="fileUpload">Upload a file:</label>
-                <input type="file" id="fileUpload" name="fileUpload"/>
-            </div>
-            <div className ="formGroup">
-                <label for="caption">Caption:</label>
-                <input type="text" id="caption" name="caption" placeholder="Enter a caption"/>
-            </div>
-            <button type="submit">Upload</button>
-        </form>
-    </div>
+    <div style = {{display: 'flex', justifyContent: 'center', paddingTop: "1em" }} >
+      <UploadFile/>
     </div>
   )
 }
